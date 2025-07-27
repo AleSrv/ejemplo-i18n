c88eef6dd0bd476b4d2763eb40e1785ffb049372
alesrv <srvf2019@gmail.com>
2025-07-27 21:48:01 +0200

    font-serif labels

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languageList = {
+    es: 'Español',
+    en: 'English'
+}
+
+export const labels = {
+    es: {
+        'nav.home': 'Inicio',
+        'nav.about': 'Acerca de',
+        'home.title': 'Bienvenidos a mi pagina i18n',
+        'about.title': 'Sobre nosotros'
+    },
+    en: {
+        'nav.home': 'Home',
+        'nav.about': 'About',
+        'home.title': 'welcome to my web i18n',
+        'about.title': 'About as'
+    },
+}
